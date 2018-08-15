2a38f1c919837f64a68e546bde7de6dc9a3a748a
tckz <at.tckz@gmail.com>
2018-08-15 22:35:02 +0900

    Initial

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import BusinessMember from './lib/BusinessMember';
+import $ from 'jquery';
+
+const withWindow = typeof window !== 'undefined';
+
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('ok');
+    }, 1000)
+})
+	.then(response => {
+    	const user = new BusinessMember('taro', 'yamada', 'G社');
+		if (withWindow) {
+			$("#mes").text(user.getName());
+		}
+    	console.log(user.getName());
+	});
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: ['babel-polyfill', './index.js'], // babel-polyfill はIE11などで必要
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.js'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {comments: /^\**!|@preserve|@license|@cc_on/},
+		  safari10: true
+        }
+      })
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
